3d4f797388f5f45be11fd1d05622591acd2a33ea
yyc <1040658628@qq.com>
2017-09-15 15:20:03 +0800

    add project

---
+++ SportsStore/SportsStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Net;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EmailOrderProcessor : IOrderProcessor
+    {
+        private EmailSettings emailSettings;
+        public EmailOrderProcessor(EmailSettings settings)
+        {
+            emailSettings = settings;
+        }
+        public void ProcessOrder(Cart cart, ShoppingDetails shoppingDetails)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                MailMessage mailMessage = new MailMessage(emailSettings.MailFromAddress,emailSettings.MailToAddress,"New Order","");
+                if(emailSettings.WriteAsFile)
+                {
+                    mailMessage.BodyEncoding = Encoding.ASCII;
+                }
+                //smtpClient.Send(mailMessage);
+            }
+        }
+    }
+
+    public class EmailSettings
+    {
+        public string MailToAddress = "1040658628@qq.com";
+        public string MailFromAddress = "1040658628@qq.com";
+        public bool UseSsl = true;
+        public string Username = "1040658628@qq.com";
+        public string Password = "";
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = true;
+        public string FileLocation = @"";
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+        public int PageSize = 4;
+        public ProductController(IProductsRepository productRespository)
+        {
+            this.repository = productRespository;
+        }
+        public ViewResult List(string category,int page = 1)
+        {
+            ProductListViewModel model = new ProductListViewModel {
+                Products = repository.Products.Where(p => p.Category == category || category == null).OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(x => x.Category == category).Count()
+                },
+                CurrentCategory = category
+            };
+            return View(model);
+        }
+
+        public FileContentResult GetImage(int ProductID)
+        {
+            Product prod = repository.Products.FirstOrDefault(p => p.ProductID == ProductID);
+            if(prod != null)
+            {
+                return File(prod.ImageData,prod.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
